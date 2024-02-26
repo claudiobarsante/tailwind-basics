1059d6f5b62148d6e086a9fe37a96ec27cd5b5e0
Claudio Barsante <clbmribas@gmail.com>
2024-02-26 12:19:13 -0300

    feat: add preview to 'Your photo' section

--- src/app/components/Form/FileInput/ImagePreview.tsx
+++ src/app/components/Form/FileInput/ImagePreview.tsx
@@ -1,11 +1,25 @@
+'use client';
 import { User } from 'lucide-react';
-
+import { useFileInput } from './Root';
+import { useMemo } from 'react';
+import Image from 'next/image';
 function ImagePreview() {
-  return (
-    <div className="flex size-16 items-center justify-center rounded-full bg-violet-50">
-      <User className="size-8 text-violet-600" />
-    </div>
-  );
+  const { files } = useFileInput();
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) return null;
+    return URL.createObjectURL(files[0]);
+  }, [files]);
+
+  if (previewURL === null) {
+    return (
+      <div className="flex size-16 items-center justify-center rounded-full bg-violet-50">
+        <User className="size-8 text-violet-600" />
+      </div>
+    );
+  }
+
+  return <Image src={previewURL} alt="" width={64} height={64} className="rounded-full object-cover" />;
 }
 
 export default ImagePreview;
