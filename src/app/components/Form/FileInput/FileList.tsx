4c63d769bf3ae341917d9c2bced25a04d92ed887
Claudio Barsante <clbmribas@gmail.com>
2024-03-04 16:08:50 -0300

    refactor: add <FileItem/> and use Tailwind variants with slots to conditionally render styles

--- src/app/components/Form/FileInput/FileList.tsx
+++ src/app/components/Form/FileInput/FileList.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React from 'react';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { formatBytes } from '@/utils/format-bytes';
-import { Trash2, UploadCloud } from 'lucide-react';
 import { useFileInput } from './Root';
-import Button from '../../Button';
+import FileItem from './FileItem';
 
 function FileList() {
   const [parent] = useAutoAnimate();
@@ -12,28 +10,7 @@ function FileList() {
   return (
     <div ref={parent} className="mt-4 space-y-3">
       {files.length > 0 &&
-        files.map((file) => (
-          <div key={file.name} className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4">
-            <div className="rounded-full border-4  border-violet-100 bg-violet-200 p-2 text-violet-600">
-              <UploadCloud className="size-4" />
-            </div>
-            <div className="flex flex-1 flex-col items-start gap-1">
-              <div className="flex flex-col">
-                <span className="text-sm font-medium text-zinc-700">{file.name}</span>
-                <span className="text-xs text-zinc-500">{formatBytes(file.size)}</span>
-              </div>
-              <div className="flex w-full items-center gap-3">
-                <div className="h-2 w-full rounded-full bg-zinc-100">
-                  <div className="h-2 w-4/5 rounded-full bg-violet-600" />
-                </div>
-                <span className="text-sm font-medium text-zinc-700">80%</span>
-              </div>
-            </div>
-            <Button type="button" variant="ghost">
-              <Trash2 className="size-5 text-zinc-500" />
-            </Button>
-          </div>
-        ))}
+        files.map((file) => <FileItem key={file.name} name={file.name} size={file.size} state="error" />)}
     </div>
   );
 }
