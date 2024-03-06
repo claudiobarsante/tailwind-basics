37be8ea4d35125745598418dd4e46d704b705477
Claudio Barsante <clbmribas@gmail.com>
2024-03-06 15:49:33 -0300

    refactor: add button to open the side bar

--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -1,38 +1,53 @@
+'use client';
 import React from 'react';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { Cog, LifeBuoy, Menu, Search } from 'lucide-react';
 import Logo from './Logo';
-import { Cog, LifeBuoy, Search } from 'lucide-react';
 import MainNavigation from './MainNavigation';
 import NavItem from './MainNavigation/NavItem';
 import UsedSpaceWidget from './UsedSpaceWidget';
 import Profile from './Profile';
 import { Input } from '../Input';
+import Button from '../Button';
 
 export default function SideBar() {
   return (
     // - Control the vertical space between elements using the space-y-{amount} utilities
     // <aside className="min-h-screen space-y-6 border-r border-zinc-200 px-5 py-8">
     // -- could use bottom-0 to fill the entire screen height
-    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
-      {/*-- composition pattern --*/}
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
-      <MainNavigation />
-      {/*mt-auto will push the content to the bottom */}
-      <div className="mt-auto flex flex-col gap-6">
-        <nav className="space-y-0.5">
-          <NavItem title="Support" icon={LifeBuoy} />
-          <NavItem title="Settings" icon={Cog} />
-        </nav>
-        <UsedSpaceWidget />
-        <div className="h-px bg-zinc-200" />
-        <Profile />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
       </div>
-    </aside>
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        {/*-- composition pattern --*/}
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </Input.Prefix>
+          <Input.Control placeholder="Search" />
+        </Input.Root>
+        <MainNavigation />
+        {/*mt-auto will push the content to the bottom */}
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
+          <UsedSpaceWidget />
+          <div className="h-px bg-zinc-200" />
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
 /*
