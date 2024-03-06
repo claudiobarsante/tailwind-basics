74de00f0dc0e0d13d31009b385a6321fd96b4528
Claudio Barsante <clbmribas@gmail.com>
2024-03-06 13:36:01 -0300

    refactor: make <Sidebar/> responsive

--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -11,7 +11,8 @@ export default function SideBar() {
   return (
     // - Control the vertical space between elements using the space-y-{amount} utilities
     // <aside className="min-h-screen space-y-6 border-r border-zinc-200 px-5 py-8">
-    <aside className="flex min-h-screen flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    // -- could use bottom-0 to fill the entire screen height
+    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
       {/*-- composition pattern --*/}
       <Input.Root>
