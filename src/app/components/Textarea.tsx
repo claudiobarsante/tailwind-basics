b27b58a3671994aa5ec900c3229900b9d5777898
Claudio Barsante <clbmribas@gmail.com>
2024-03-12 11:06:37 -0300

    feat: add dark theme <Textarea/>

--- src/app/components/Textarea.tsx
+++ src/app/components/Textarea.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type TextareaProps = ComponentProps<'textarea'>;
 
@@ -9,7 +10,12 @@ function Textarea(props: TextareaProps) {
   return (
     <textarea
       {...props}
-      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100"
+      className={twMerge(
+        'min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2',
+        'shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus:border-violet-500',
+        'dark:text-zinc-100 dark:placeholder-zinc-400 dark:focus:ring-violet-500/20'
+      )}
     />
   );
 }
