f406e37195c5d8eb0000ff86e278eaf0b1878070
Claudio Barsante <clbmribas@gmail.com>
2024-03-12 11:22:19 -0300

    feat: add dark theme <Select/>

--- src/app/components/Select/index.tsx
+++ src/app/components/Select/index.tsx
@@ -2,7 +2,7 @@
 import { ReactNode } from 'react';
 import * as SelectPrimitive from '@radix-ui/react-select';
 import { ChevronDown } from 'lucide-react';
-
+import { twMerge } from 'tailwind-merge';
 type SelectProps = {
   children: ReactNode;
   placeholder: string;
@@ -11,7 +11,14 @@ type SelectProps = {
 function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center   justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
+      <SelectPrimitive.Trigger
+        className={twMerge(
+          'flex h-11 w-full items-center   justify-between gap-2 rounded-lg border',
+          'border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300',
+          'focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600',
+          'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20 dark:data-[placeholder]:text-zinc-400'
+        )}
+      >
         <SelectPrimitive.Value placeholder={placeholder} className="text-black" />
         <SelectPrimitive.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
@@ -23,7 +30,7 @@ function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white"
+          className="z-10 w-[--radix-select-trigger-width] animate-slideDownAndFade overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm dark:border-zinc-700 dark:bg-zinc-800"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
